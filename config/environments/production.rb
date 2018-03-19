78b893b07f9a26e03ed9b8a978404923744d01b4
Freddy <facuellarg@unal.edu.co>
2018-03-19 02:37:20 -0500

    heroku production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,22 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options =   { :host => 'https://pacific-coast-85344.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+        address: "smtp.gmail.com",
+        port: 587,
+        authentication: "plain",
+        enable_starttls_auto: true,
+        user_name: "facg995@gmail.com",
+        password: "meryluz95" 
+
+    }
+    config.action_mailer.default_url_options = { host: 'https://pacific-coast-85344.herokuapp.com' }
+
+
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
